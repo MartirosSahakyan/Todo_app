d34ef86bf3ca20b8827bf3fdd93b57eb7ff56173
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-07-20 17:39:41 +0400

    feat(Todo.jsx): add complete handler and css

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createNewId } from "../../helpers/helper";
 import { Input } from "../Input/Input";
-
+import "./Todo.css";
 export default class Todo extends React.Component {
   state = {
-    todos: [{ id: createNewId(), text: "Learn HTML" }],
+    todos: [{ id: createNewId(), text: "Learn HTML", isComplete: false }],
     todoInput: "",
     isDisable: true,
   };
@@ -14,7 +14,7 @@ export default class Todo extends React.Component {
       return {
         todos: [
           ...prevState.todos,
-          { id: createNewId(), text: prevState.todoInput },
+          { id: createNewId(), text: prevState.todoInput, isComplete: false },
         ],
         todoInput: "",
         isDisable: true,
@@ -22,16 +22,13 @@ export default class Todo extends React.Component {
     });
   };
   handleInputChange = (e) => {
-      if(e.target.value.trim()){
-          this.setState({ todoInput: e.target.value, isDisable: false });
-        }else{
-            this.setState({ todoInput: '', isDisable: true });
-        }
-
-
-        
-
+    if (e.target.value.trim()) {
+      this.setState({ todoInput: e.target.value, isDisable: false });
+    } else {
+      this.setState({ todoInput: "", isDisable: true });
+    }
   };
+
   handleDeleteTodo = (id) => {
     this.setState((prevState) => {
       return {
@@ -40,8 +37,17 @@ export default class Todo extends React.Component {
     });
   };
 
+  handleComplete = (id) => {
+    this.setState(({ todos }) => ({
+        todos: todos.map((todo) =>
+          todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
+        ),
+      }));
+  };
+
   render() {
     const { todos, todoInput, isDisable } = this.state;
+    // console.log(this.state.todos[0].isComplete);
     return (
       <>
         <Input value={todoInput} onChange={this.handleInputChange} />
@@ -49,10 +55,12 @@ export default class Todo extends React.Component {
           ADD TODO
         </button>
         <ul>
-          {todos.map(({ text, id }) => {
+          {todos.map(({ text, id, isComplete }) => {
             return (
-              <li key={id}>
-                <span>{text}</span>
+              <li key={id} >
+                <span onClick={()=>this.handleComplete(id)} className={isComplete ? "checked" : "unChecked"}>
+                  {text}
+                </span>
                 <button>Edit</button>
                 <button onClick={() => this.handleDeleteTodo(id)}>
                   Delete
