9b70b8b1369dc15dc2294ee9e0056295294895b7
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-07-22 17:13:14 +0400

    fix(helper.js): fix idGenerator

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,12 +1,13 @@
-function idGenerator() {
-    let id = 0
-    return () =>{
-        return id+= 1 
-    }
-}
-const createNewId = idGenerator()
+// function idGenerator() {
+//     let id = 0
+//     return () =>{
+//         return id+= 1
+//     }
+// }
+// const createNewId = idGenerator()
 
+const createNewId = () => {
+  return Date.now();
+};
 
-
-
-export {createNewId}
+export { createNewId };
