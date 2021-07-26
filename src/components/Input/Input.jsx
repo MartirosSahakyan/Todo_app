c7f62c3f3ddf2f116b3d7563b79f8dceb11a4dd5
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-07-26 13:48:12 +0400

    feat(Input.jsx): add autoFocus

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -14,6 +14,7 @@ export function Input({ onChange, value, placeholder, name }) {
       onChange={onChange}
       value={value}
       placeholder={placeholder}
+      autoFocus={true}
     />
   );
 }
